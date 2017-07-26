6abbd8c3ec2da4babbcf1ee712d64bf5f9342e42
AustinLeeParker <tehdemopan@gmail.com>
2017-07-26 14:09:16 -0500

    Made functions for max of two functions

--- starter_files/main.js
+++ starter_files/main.js
@@ -2,44 +2,78 @@
 // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
 // Then, write and example of using the function.
 
-function max(){
-    // Your answer here
-}
-
+// function max(num1, num2) {
+//     // Your answer here
+//     if (num2 > num1) {
+//         return num2;
+//     }
+//     else if (num1 === num2) {
+//         return "they are equal";
+//     }
+//     else {
+//         return num1;
+//     }
+// }
+// console.log(max(10,0));
 
 // 2.
 // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
 // Then, write and example of using the function.
 
-function maxOfThree(){
-    // Your answer here
-}
+// function maxOfThree(num1, num2, num3) {
+//     // Your answer here
+//     var max = 0;
+//     if ((num1 >= num2) && (num1 >= num3)) {
+//         max = num1;
+//     }
+//     else if ((num2 >= num1) && (num2 >= num3)) {
+//         max = num2;
+//     }
+//     else {
+//         max = num3;
+//     }
+//     return max;
+// }
+// console.log(maxOfThree(5, 10, 20));
 
 
 // 3.
 // Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
 // Then, write and example of using the function.
 
-function isVowel(char){
+function isVowel(char(a, e, i, o, u)) {
     // Your answer here
+
+
 }
 
 
 // 4.
 // Write a function called `sum` that takes two parameters and returns the sum of those 2 numbers.
 // Then, write and example of using the function.
-
+// function sum(num1, num2) {
+//     if sum(num1, num2) {
+//         return "true";
+//     }
+// }
 
 
 // 5.
 // Write a function named `avg` that takes 3 parameters and returns the average of those 3 numbers.
 // Then, write and example of using the function.
-
+// function avg(num1, num2, num3) {
+//     if avg(num1.num2, num3){
+//         return "avg";
+//     }
+// }
 
 
 // 6.
 // Write a function called `getLength` that takes one parameter (a string) and returns the length
 // Then, write and example of using the function.
+// function getLength("Hello"){
+
+// }
 
 
 
